c30282bf695b12840cd22a6eb27faa2c790a46cd
fcrab <evarasseed@gmail.com>
2013-03-12 17:23:29 +0800

    RTTIdemo

--- fcJavaDemo/src/info/kikiFcrab/CustomException/CustomExceptions.java
+++ fcJavaDemo/src/info/kikiFcrab/CustomException/CustomExceptions.java
@@ -4,22 +4,23 @@ public class CustomExceptions {
 
 	/**
 	 * @param args
+	 * 自定义异常抛出&&栈信息打印
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try{
-			throw new Exception("Test Exception");
-		}catch(Exception e){
-			System.out.println("Caught Exception");
-			//base Class：throwable
-			System.out.println("get Message():"+e.getMessage());
-			System.out.println("getLocalizedMessage():"+e.getLocalizedMessage());
-			System.out.println("toString():"+e.toString());
-			System.out.print("printStackTrace():");
-			e.printStackTrace(System.out);
-		}
-		
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		try{
+//			throw new Exception("Test Exception");
+//		}catch(Exception e){
+//			System.out.println("Caught Exception");
+//			//base Class：throwable
+//			System.out.println("get Message():"+e.getMessage());
+//			System.out.println("getLocalizedMessage():"+e.getLocalizedMessage());
+//			System.out.println("toString():"+e.toString());
+//			System.out.print("printStackTrace():");
+//			e.printStackTrace(System.out);
+//		}
+//		
+//	}
 	
 	class SampleException extends Exception{
 		
---
+++ fcJavaDemo/src/info/kikiFcrab/RTTI/RTTIdemo.java
@@ -0,0 +1,44 @@
+package info.kikiFcrab.RTTI;
+
+import java.util.*;
+
+//利用RTTI创建类型数组
+
+public class RTTIdemo<T> {
+
+	private Class<T> type;
+	public RTTIdemo(Class<T> type){
+		this.type=type;
+	}
+	public List<T> create(int nElements){
+		List<T> result=new ArrayList<T>();
+		try{
+			for(int i=0;i<nElements;i++){
+				result.add(type.newInstance());
+			}
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RTTIdemo<CountedInteger> fl=
+				new RTTIdemo<CountedInteger>(CountedInteger.class);
+		System.out.println(fl.create(15));
+	}
+
+}
+
+//自定义的数组类
+class CountedInteger{
+	private static long count;
+	private final long id=count++;
+	public String toString(){
+		return Long.toString(id);
+	}
+}
